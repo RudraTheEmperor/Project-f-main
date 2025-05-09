212d6ceaf6e2a1f115461f5ba3d5d8c43fbd9604
RudraTheEmperor <143614564+RudraTheEmperor@users.noreply.github.com>
2025-05-09 20:56:56 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -2,7 +2,7 @@
 
     $servidor = "localhost";
     $usuario = "root";
-    $senhaaaaaa = "";
+    $senha = "";
     $dbname = "primeira_aula";
 
     $conn = new mysqli($servidor,$usuario,$senha,$dbname);
